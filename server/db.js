55d2ad53f2299f76e0d200532b7ed149a14cefc6
ShotursunovShomurod <ziyodxon.i08@gmail.com>
2024-07-04 19:38:11 +0500

    telegramChat

---
+++ server/db.js
@@ -0,0 +1,15 @@
+export const DATA = [
+    "Assalomu Alaykum",
+    "nma qvossiz ukam",
+    "Messi qale",
+    "qattasiz",
+    "tinch Rahmat Ollohga shukur",
+    "Sizga ish bor edi",
+    "Al Nassrga kemisizmi ?",
+    "Bopti gapni aylantirib otirmiman",
+    "Real Madrid bilan 5 yillik shartnomaga nima disiz ?",
+    "Hop",
+    "Yozvorolisizmi manga",
+    "yoge",
+    "man ham",
+]
